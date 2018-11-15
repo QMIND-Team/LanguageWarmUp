05ff8c627e60771d1fdb21d2c077b07e2f7ce7c7
Olivia <16kot1@queensu.ca>
2018-11-15 13:26:23 -0500

    added countvectorizer

--- FeatureCreate/feature.py
+++ FeatureCreate/feature.py
@@ -7,6 +7,9 @@ Normallized vectors, ready to be passed into a neural net
 '''
 import random
 
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
+
 with open("words.txt") as f:
     words = f.readlines()
 words = [x.strip() for x in words]
@@ -18,13 +21,10 @@ for i in range (0,10):
     for j in range(0,phraselength):
         choice = random.randint(0,len(words))
         phrase.append(words[choice])
-    phrases.append(phrase)
+        sentence = ' '.join(phrase)
+    phrases.append(sentence)
 
 print(phrases)
-
-
-
-
 
 '''
 Some ideas:
@@ -32,3 +32,12 @@ Let's use Bag of Words
 Are there any libraries that have this built in?  (Almost certainly yes!)
 How do I give those functions my data (Probably a Pandas Dataframe!)
 '''
+#wasn't working properly without lowercase set to false, I thint it has to do with apostrophes
+#binary set to false will give us a count of how many times that word was present in the sentence
+
+vectorizer = CountVectorizer(binary=True, lowercase=false)
+vector = vectorizer.fit_transform(phrases)
+
+print( vector.todense() )
+print( vectorizer.vocabulary_ )
+
